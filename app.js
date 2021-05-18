5937f5141b7bc8156078b35e410a621f11694970
Lug <83981636+lug19@users.noreply.github.com>
2021-05-18 11:37:28 -0300

    final
    aun me queda la duda en app.js, por que no puedo concatenar el mensaje con $ en su clase del mensaje

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ class UI {
         element.innerHTML = `
         <div class="card text-center mb-4">
             <div class="card-body">
-                <strong>Product Name</strong>: ${product.name}
-                <strong>Product Price</strong>: ${product.price}
+                <strong>Product Name</strong>: ${product.name} -
+                <strong>Product Price</strong>: ${product.price} -
                 <strong>Product Year</strong>: ${product.year}
                 <a href='#' class='btn btn-danger' name="delete">Delete</a>
             </div>
@@ -28,6 +28,7 @@ class UI {
     deleteProduct(element){
         if(element.name === 'delete'){
             element.parentElement.parentElement.parentElement.remove();
+            this.showMessage('Product Delete Successfully', 'info');
         }
 
 
@@ -35,7 +36,8 @@ class UI {
 
     showMessage(message, cssClass){
         const div = document.createElement('div');
-        div.className= 'alert alert-${cssClass} mt-2';
+        console.log(cssClass);
+        div.className = 'alert mt-2 alert-' + cssClass;
         div.appendChild(document.createTextNode(message));
         //show in the DOM
         const container = document.querySelector('.container');
@@ -58,6 +60,10 @@ document.getElementById('product-form')
     
     const product = new Product(name, price, year);
     const ui = new UI();
+
+    if(name===''|| price ==='' || year ===''){
+      return ui.showMessage('Complete fields Please', 'danger');
+    }
     ui.addProduct(product);
     ui.resetForm();
     ui.showMessage('Product Added successfully', 'success');
